e6f04d5c85016eb58b7c1c9e629a3360a12fc1b0
Sanskar Shukla <sanskar0627@gmail.com>
2026-02-22 18:49:51 +0530

    set up

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const page =()=>{
+  return(
+    <div className="text-red-600 font-bold items-center" >
+      hello sanskar
+      <Button>
+        click me 
+      </Button>
+    </div>
+  );
+};
+export default page;
