c02f278bc404bd1e24e86d6263b651694a0b10a2
Sanskar Shukla <sanskar0627@gmail.com>
2026-07-25 11:03:00 +0530

    wire up prisma to trpc router, db actually returns data now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
-import { Button } from "@/components/ui/button";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
-const page =()=>{
-  return(
-    <div className="text-red-600 font-bold items-center" >
-      hello sanskar
-      <Button>
-        click me 
-      </Button>
+const Page = async () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>loading...</p>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 };
-export default page;
+
+export default Page;
