d2f0675fa7fba97798229b452fd4359e2cf992bb
Sanskar Shukla <sanskar0627@gmail.com>
2026-07-27 15:37:00 +0530

    fix auth-client, old file had server code in it for some reason

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,13 @@
+import { RegisterForm } from "@/features/auth/components/Register-form";
+
+ 
+ const page=()=>{
+    return(
+        <div>
+            <RegisterForm>
+                
+            </RegisterForm>
+        </div>
+    )
+}
+export default page;
