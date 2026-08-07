8271263416bc2dde591427633c1ad85f19e31a86
Sanskar Shukla <sanskar0627@gmail.com>
2026-08-07 14:23:17 +0530

    add polar billing integration, wire up checkout and portal plugins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,35 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
 import prisma from "@/lib/db";
-
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
-    }),
-    emailAndPassword: {
-        enabled: true,
-        autoSignIn :true,
-    }
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      use: [
+        checkout({
+          products: [
+            {
+              productId:
+                "231a4322-30b1-4c0f-b08f-0b409b64e258",
+              slug: "pro",
+            }
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    })
+  ]
 });
